76c22c076612d6ab3d8fefb8d3e7e6f9d3f9c465
Mohan7471 <skmohanraj7471@gmail.com>
2025-02-12 16:35:10 +0530

    Update Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -9,7 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo1 {
    public static void main(String[] args) throws InterruptedException {
-      WebDriver driver = new ChromeDriver();
+      WebDriver driver = new ChromeDriver();// open the browser
+      //chromedriver driver=new chromedriver();//using same class name
       driver.manage().window().maximize();
       driver.manage().deleteAllCookies();
       driver.get("https://www.google.co.in/");
@@ -23,22 +24,22 @@ public class Demo1 {
       System.out.println(driver.getCurrentUrl());
       driver.navigate().back();
       driver.navigate().forward();
-      Thread.sleep(4000L);
+      Thread.sleep(4000);
       String singlewindow = driver.getWindowHandle();
       System.out.println(singlewindow);
       driver.navigate().to("https://demo.automationtesting.in/Windows.html");
       driver.findElement(By.xpath("(//button[@class='btn btn-info'][normalize-space()='click'])[1]")).click();
-      Set<String> child = driver.getWindowHandles();
-      ArrayList<String> al = new ArrayList(child);
+      Set<String> child = driver.getWindowHandles();//no get method in set
+      ArrayList<String> al = new ArrayList(child);//get method in list only
       driver.switchTo().window((String)al.get(1));
       Set<String> child1 = driver.getWindowHandles();
       System.out.println(child1);
       System.out.println(driver.getCurrentUrl());
       driver.switchTo().window((String)al.get(0));
-      driver.close();
+      driver.close();//close only current tab
       driver.switchTo().window((String)al.get(1));
-      Thread.sleep(4000L);
+      Thread.sleep(4000);
       driver.manage().window().minimize();
-      driver.quit();
+      driver.quit();//close multiple tab
    }
 }
